9bf8279085a87e5c8f9cbc90e212c8d5219fa0bb
Johann Sealtiel Bleza <johann.bleza@icloud.com>
2025-07-14 19:37:04 +0800

    Added favicon, Updated navbar padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white w-full shadow p-4 text-xl font-bold ">
+    <nav className="bg-white w-full shadow px-4 py-3 text-xl font-bold ">
       <Link href="/" className="flex gap-2 items-center max-w-[50rem] mx-auto">
         <Calendar />
         <h1>SchedMo</h1>
